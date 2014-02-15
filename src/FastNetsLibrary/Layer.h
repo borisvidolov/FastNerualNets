aa8904f0aa37c14607ba4dd53dc90a1df1a7de44
Boris Vidolov <boris@vidolov.com>
2014-02-15 00:03:07 -0800

    some more code

--- src/FastNetsLibrary/Layer.h
+++ src/FastNetsLibrary/Layer.h
@@ -1,7 +1,25 @@
+// Created by Boris Vidolov on 02/14/2014
+// Published under Apache 2.0 licence.
 #pragma once
-template<int INPUT, int OUTPUT>
+#include <Windows.h>
+
+namespace FasteNets
+{
+
+/* Represents a single layer in the network */
+template<int INPUT, int OUTPUT, class FloatingPoint = double>
 class Layer
 {
+	_CRT_ALIGN(32) FloatingPoint Weights[OUTPUT][INPUT];
+	_CRT_ALIGN(32) FloatingPoint ReverseWeights[INPUT][OUTPUT];//Cache for faster calculation
+	_CRT_ALIGN(32) FloatingPoint B[OUTPUT];//Input Bias
+	_CRT_ALIGN(32) FloatingPoint C[INPUT];//Output Bias
+
+	CRITICAL_SECTION ReverseLock;
+	bool  mReverseWeightsDirty;
+private:
+	Layer(const Layer&){}//No copy
+
 public:
 
 	Layer(void)
@@ -13,3 +31,4 @@ public:
 	}
 };
 
+}//Fast nets
--- src/FastNetsTestApp/FastNetsTestApp.cpp
+++ src/FastNetsTestApp/FastNetsTestApp.cpp
@@ -2,10 +2,13 @@
 //
 
 #include "stdafx.h"
+#include "..\FastNetsLibrary\Layer.h"
 
+using namespace FasteNets;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Layer<10, 10> l;
 	return 0;
 }
 
